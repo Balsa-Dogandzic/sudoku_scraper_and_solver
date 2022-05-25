7346b61218ca6d1371214209fe3dc4e2fd9ad8e8
Balsa-Dogandzic <100625564+Balsa-Dogandzic@users.noreply.github.com>
2022-05-25 17:55:09 +0200

    Making solving algorithm

--- sss/scraping.py
+++ sss/scraping.py
@@ -1,4 +1,5 @@
 """Module for scraping of the puzzle and solving the scraped puzzle"""
+from copy import deepcopy
 from bs4 import BeautifulSoup
 import requests
 
@@ -17,7 +18,7 @@ def scrape():
 
     for i in range(9):
         puzzle.append([])
-        for j in range(9):
+        for _ in range(9):
             if input_fields[index].attrs.get('value') is not None:
                 puzzle[i].append(int(input_fields[index].attrs['value']))
             else:
@@ -27,4 +28,57 @@ def scrape():
     return puzzle
 
 
-print(scrape())
+def find_next_empty(puzzle):
+    """Function finds the next 0 in the puzzle"""
+    for i in range(9):
+        for j in range(9):
+            if puzzle[i][j] == 0:
+                return i, j
+
+    return None, None
+
+
+def is_valid(puzzle, guess, row, col):
+    """Function checks if a number can be inserted in that position"""
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False
+
+    col_vals = [puzzle[i][col] for i in range(9)]
+    if guess in col_vals:
+        return False
+
+    row_start = (row // 3) * 3
+    col_start = (col // 3) * 3
+
+    for i in range(row_start, row_start + 3):
+        for j in range(col_start, col_start + 3):
+            if puzzle[i][j] == guess:
+                return False
+
+    return True
+
+
+def solve_sudoku(puzzle):
+    """Function solves the puzzle and returns it"""
+    row, col = find_next_empty(puzzle)
+
+    if row is None:
+        return puzzle
+
+    for guess in range(1, 10):
+        if is_valid(puzzle, guess, row, col):
+            puzzle[row][col] = guess
+            if solve_sudoku(puzzle):
+                return puzzle
+
+        puzzle[row][col] = 0
+
+    return False
+
+
+sudoku = scrape()
+temp = deepcopy(sudoku)
+solved = solve_sudoku(temp)
+
+print(solved)
