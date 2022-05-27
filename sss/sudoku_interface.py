58790321d21f0302fe0a1771cca0328b46a635d2
Balsa-Dogandzic <100625564+Balsa-Dogandzic@users.noreply.github.com>
2022-05-27 12:38:25 +0200

    Enabling input buttons

--- sss/sudoku_interface.py
+++ sss/sudoku_interface.py
@@ -1,5 +1,5 @@
 """In this module is code for interface and all it's functions"""
-from tkinter import Entry, Tk, Label, Button
+from tkinter import Entry, Tk, Label, Button, END
 
 root = Tk()
 root.title("Sudoku")
@@ -32,19 +32,39 @@ def draw9x9():
 
 draw9x9()
 
-clear_button = Button(text="Clear", width=10)
+clear_button = Button(text="Clear", width=10,
+                      command=lambda: write_number("del"))
 clear_button.grid(row=15, column=1, columnspan=5, pady=20)
 
 solve_button = Button(text="Solve", width=10)
 solve_button.grid(row=15, column=5, columnspan=5, pady=20)
 
 
+def write_number(argument):
+    """Functions enables buttons to write numbers on the board"""
+    focused_entry = root.focus_get()
+    focused_entry.delete(0, END)
+    if argument != "del":
+        focused_entry.insert(0, argument)
+
+
 def show_btns():
     """Function inserts buttons for number input"""
+    buttons = []
     for i in range(1, 10):
         num_button = Button(root, text=i)
         num_button.grid(row=20, column=i, sticky="nsew",
                         padx=1, pady=1, ipady=5)
+        buttons.append(num_button)
+    buttons[0].configure(command=lambda: write_number(1))
+    buttons[1].configure(command=lambda: write_number(2))
+    buttons[2].configure(command=lambda: write_number(3))
+    buttons[3].configure(command=lambda: write_number(4))
+    buttons[4].configure(command=lambda: write_number(5))
+    buttons[5].configure(command=lambda: write_number(6))
+    buttons[6].configure(command=lambda: write_number(7))
+    buttons[7].configure(command=lambda: write_number(8))
+    buttons[8].configure(command=lambda: write_number(9))
 
 
 show_btns()
